3f76233beaf1f43e314bfcf0be852a2e4be2c4bf
RoSolis <150153217+Rodrigo5036@users.noreply.github.com>
2025-06-13 23:33:39 -0400

    Actualización desde IA

---
+++ src/src.ino
@@ -0,0 +1,20 @@
+#include "ColabiOTA.h"
+#include <WiFi.h>
+
+const char* ssid = "TU_SSID";
+const char* password = "TU_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando a WiFi...");
+  }
+  Serial.println("Conexión WiFi exitosa!");
+  ColabiOTA::begin();
+}
+
+void loop() {
+  ColabiOTA::handle();
+}

54e8403de36d913658b8323fff1272d39f653ac6
RoSolis <150153217+Rodrigo5036@users.noreply.github.com>
2025-06-16 12:05:13 -0400

    Actualización desde IA

---
+++ src/src.ino
@@ -0,0 +1,19 @@
+```cpp
+#include <ColabiOTA.h>
+
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  ColabiOTA::begin();
+}
+
+void loop() {
+  ColabiOTA::handle();
+  
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
+```

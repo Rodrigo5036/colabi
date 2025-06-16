1d999a4c8dafd83d8b5c7eccedb935c21761cbc8
RoSolis <150153217+Rodrigo5036@users.noreply.github.com>
2025-06-16 12:59:14 -0400

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,26 +1,27 @@
 #include "ColabiOTA.h"
-#include <Servo.h>
+#include <ESP32Servo.h>
 
-Servo motor;
-unsigned long previousMillis = 0;
-const long interval1 = 1000; // 1 segundo
-const long interval2 = 2000; // 2 segundos
-bool moveFast = true;
+static const int servoPin = 13;
+
+Servo servo1;
 
 void setup() {
   ColabiOTA::begin();
-  motor.attach(18); // Pin donde está conectado el servo (cambiar a un pin compatible con ESP32)
-  motor.write(0); // Posición inicial
+  servo1.attach(servoPin);
 }
 
 void loop() {
   ColabiOTA::handle();
   
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= (moveFast ? interval1 : interval2)) {
-    previousMillis = currentMillis;
-    motor.write(90); // Mover el motor a 90 grados
-    delay(1000); // Esperar un segundo
-    motor.write(0); // Regresar el motor a 0 grados
+  for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+    servo1.write(posDegrees);
+    Serial.println(posDegrees);
+    delay(20);
+  }
+
+  for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
+    servo1.write(posDegrees);
+    Serial.println(posDegrees);
+    delay(20);
   }
 }

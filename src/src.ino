89f8d1463f1e89a96ed05a97a6de1cba5a637b0d
RoSolis <150153217+Rodrigo5036@users.noreply.github.com>
2025-06-17 12:45:05 -0400

    Actualización automática desde Python para src/src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,18 +1,40 @@
-#include "ColabiOTA.h"
 #include <ESP32Servo.h>
+#include <Adafruit_NeoPixel.h>
 
-Servo myServo;
+#define LED_PIN 15
+#define SERVO_PIN 2
+
+Servo myservo;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+#include "ColabiOTA.h"
 
 void setup() {
   ColabiOTA::begin();
-  myServo.attach(18); // Pin GPIO 18, cambiar según configuración
-  myServo.write(90); // Posición inicial del servo a 90 grados
+  
+  myservo.attach(SERVO_PIN);
+  strip.begin();
+  strip.show();
+  
+  myservo.write(0);  // Inicializa el servo en la posición 0 grados
+  strip.setPixelColor(0, strip.Color(0, 0, 0));  // Apaga el LED
+  strip.show();
 }
 
 void loop() {
   ColabiOTA::handle();
-  delay(2000); // Pausa de 2 segundos
-  myServo.write(0); // mueve el servo a 0 grados
-  delay(2000); // Pausa de 2 segundos
-  myServo.write(180); // mueve el servo a 180 grados
+  
+  for (int pos = 0; pos <= 180; pos += 1) {
+    myservo.write(pos);
+    strip.setPixelColor(0, strip.Color(0, 255, 0));  // Enciende el LED en verde
+    strip.show();
+    delay(15);
+  }
+
+  for (int pos = 180; pos >= 0; pos -= 1) {
+    myservo.write(pos);
+    strip.setPixelColor(0, strip.Color(255, 0, 0));  // Cambia el LED a rojo
+    strip.show();
+    delay(15);
+  }
 }

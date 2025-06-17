dce2f219751af5b09da540bd3ac97d02df618082
RoSolis <150153217+Rodrigo5036@users.noreply.github.com>
2025-06-17 12:55:14 -0400

    Actualización automática desde Python para src/src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,40 +1,36 @@
-#include <ESP32Servo.h>
-#include <Adafruit_NeoPixel.h>
-
-#define LED_PIN 15
-#define SERVO_PIN 2
-
-Servo myservo;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
-
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
 #include "ColabiOTA.h"
 
+const char* ssid = "tuSSID";
+const char* password = "tuContrasena";
+
+AsyncWebServer server(80);
+
 void setup() {
-  ColabiOTA::begin();
-  
-  myservo.attach(SERVO_PIN);
-  strip.begin();
-  strip.show();
-  
-  myservo.write(0);  // Inicializa el servo en la posición 0 grados
-  strip.setPixelColor(0, strip.Color(0, 0, 0));  // Apaga el LED
-  strip.show();
+    ColabiOTA::begin();
+    
+    // Conectar a la red WiFi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+    }
+    
+    // Configurar el servidor para servir subtítulos
+    server.on("/subtitulos", HTTP_GET, [](AsyncWebServerRequest *request){
+        StaticJsonDocument<200> doc;
+        doc["subtitulos"] = "Subtítulos realizados por la comunidad de Amara.org";
+        
+        String output;
+        serializeJson(doc, output);
+        request->send(200, "application/json", output);
+    });
+    
+    // Iniciar el servidor
+    server.begin();
 }
 
 void loop() {
-  ColabiOTA::handle();
-  
-  for (int pos = 0; pos <= 180; pos += 1) {
-    myservo.write(pos);
-    strip.setPixelColor(0, strip.Color(0, 255, 0));  // Enciende el LED en verde
-    strip.show();
-    delay(15);
-  }
-
-  for (int pos = 180; pos >= 0; pos -= 1) {
-    myservo.write(pos);
-    strip.setPixelColor(0, strip.Color(255, 0, 0));  // Cambia el LED a rojo
-    strip.show();
-    delay(15);
-  }
+    ColabiOTA::handle();
 }
